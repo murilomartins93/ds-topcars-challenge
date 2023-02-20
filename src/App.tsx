5963f2b7554c6cee6d771250bb3acd00a46c0798
Murilo Martins <murilomartins@outlook.com>
2023-02-20 16:08:55 -0300

    :construction: Comment Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import CarCard from "./components/CarCard";
+import CommentCard from "./components/CommentCard";
 import Header from "./components/Header";
 
 function App() {
@@ -7,12 +8,24 @@ function App() {
     <>
       <Header />
       <main>
-        <section id="ct-cars-cardlist">
+        <section id="ct-cardlist-section">
           <h1 className="ct-cars-cardlist-title">Venha nos visitar</h1>
-          <CarCard />
-          <CarCard />
+          <div className="ct-cars-cardlist">
+            <CarCard />
+            <CarCard />
+          </div>
         </section>
-        <section id="ct-comments"></section>
+        <section id="ct-comments-section">
+          <div className="ct-cars-comments-title">O que estão dizendo</div>
+          <div>
+            <CommentCard />
+            <CommentCard />
+            <CommentCard />
+            <CommentCard />
+            <CommentCard />
+            <CommentCard />
+          </div>
+        </section>
       </main>
     </>
   );
---
+++ src/components/CommentCard/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+function CommentCard() {
+  return (
+    <>
+      <div className="ct-comment-card ct-container">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum
+          voluptatem ad fugit id distinctio.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default CommentCard;
